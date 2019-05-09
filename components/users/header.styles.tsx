f02e9b52bb1aceccd0701a8af07195e1b2ebf059
Moisés Gómez Rubio <MOISESG@ocu.es>
2019-05-09 17:36:06 +0200

    4 commit

---
+++ components/users/header.styles.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const Id = styled.th`
+
+border: 2px dotted purple;
+`;
+
+
+export const Avatar = styled.th`
+
+border: 2px solid blue;
+`;
--- components/users/header.tsx
+++ components/users/header.tsx
@@ -1,11 +1,9 @@
-import '../../styles/global-styles.css';
-const styles = require('./header.css');
+import * as s from './header.styles';
 
 export const Header = () => (
     <tr>
-      <th className="blue-box">Avatar</th>
-      <th className={styles.purpleBox}>Id</th>
-      <th>Id</th>
+      <s.Avatar>Avatar</s.Avatar>      
+      <s.Id>Id</s.Id>
       <th>Name</th>
     </tr>
   );
